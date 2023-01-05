9ab3fb1bcd2d55e91ce6c6f10e2a8a71e3e418f7
mariecp27 <mariaher.cp@gmail.com>
2023-01-04 23:24:41 -0500

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/style.scss';
 
