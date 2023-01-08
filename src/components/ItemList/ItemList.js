12c55ce341be2117445f3d54e5efdc29416237a9
mariecp27 <mariaher.cp@gmail.com>
2023-01-07 21:11:31 -0500

    Title added

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Item from "../Item/Item";
 
 function ItemList( { products } ) {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
-        <div className="item-list">
-            <h1>Nuestros Patitos</h1>
+        <div
+            className="item-list"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-once="true">
+            <img src="/duck-kingdom/assets/images/title.png" alt="title" className="item-list_title" />
             <hr />
             <div className="container-fluid">
                 <section className="row row-cols-lg-5 justify-content-center m-0">
