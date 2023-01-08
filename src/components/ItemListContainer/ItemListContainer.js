c09e780f44ce8f94442bb1678932ab0a1f8d32d5
mariecp27 <mariaher.cp@gmail.com>
2023-01-08 00:12:41 -0500

    Component for no produts after searching by name

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,23 +3,29 @@ import { useParams } from "react-router-dom";
 import { requestAllProducts } from "../../helpers/requestData";
 import Spinner from "../Spinner/Spinner";
 import ItemList from "../ItemList/ItemList";
+import NoProducts from "../NoProducts/NoProducts";
 
 function ItemListContainer() {
 
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { itemName, categoryId } = useParams();
 
     useEffect( () => {
         setProducts([]);
+        setLoading(true);
         requestAllProducts()
             .then( (res) => {
                 if (itemName) {
                     let name = itemName.toLowerCase();
                     setProducts(res.filter( product => product.name.toLowerCase().includes(name)));
+                    setLoading(false);
                 } else if (categoryId) {
                     setProducts(res.filter( product => product.category === categoryId ));
+                    setLoading(false);
                 } else {
                     setProducts(res);
+                    setLoading(false);
                 }
             })
             .catch( (err) => {
@@ -30,9 +36,11 @@ function ItemListContainer() {
     return (
         <div>
             {
-                products.length === 0
+                loading
                     ? <Spinner />
-                    : <ItemList products={products} />
+                    : products.length === 0
+                        ? <NoProducts />
+                        : <ItemList products={products} />
             }
         </div>
     );
---
+++ src/components/NoProducts/NoProducts.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function NoProducts() {
+
+    const navigate = useNavigate();
+
+    const handleHome = () => {
+        navigate("/");
+    }
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <div
+            className="not-products"
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            data-aos-once="true">
+            <img src="/duck-kingdom/assets/images/ups.png" alt="No products"/>
+            <h3>¡Oh!, de momento, no hay patitos con el nombre o categoría ingresados</h3>
+            <button className="item-detail__text-container-back" onClick={handleHome}><FontAwesomeIcon icon = { faCircleArrowLeft } />Ir al inicio</button>
+        </div>
+    )
+}
+
+export default NoProducts;
